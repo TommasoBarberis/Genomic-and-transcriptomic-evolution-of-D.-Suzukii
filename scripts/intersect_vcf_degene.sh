03a0676ec3d9aee16424f3d7c4ba7a98c7206e6f
TommasoBarberis <74647948+TommasoBarberis@users.noreply.github.com>
2021-12-14 21:51:00 +0100

    Update intersect_vcf_degene.sh

--- scripts/intersect_vcf_degene.sh
+++ scripts/intersect_vcf_degene.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+## author: Tommaso Barberis
+## date: 03/122/2021
+## description: short script that given a list of genes and a vcf file as input and an another vcf file name as output, returns in this last file SNPs that are founded in the gene list.
+
+
 list_gene="/data/home/tbarberis/projet_S3/localdata/RNAseq/DE_gene/fruit_G0.txt"
 
+# recopy header 
 grep "#" $1 >> $2
 
 cat $list_gene | while read CONTIG; do
 	grep -v "#" $1 | grep $CONTIG >> $2
-#	echo $new_lines #>> $2
-#	if [[ -n $new_lines ]]; then
-#		echo $new_lines >> $2
-#	fi
 done
-
-# cat *.out.recode.vcf >> $2
-#rm -r *.out*
